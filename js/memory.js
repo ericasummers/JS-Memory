7f82debb2b94bcf7811a3b30b5a397efb0b422f0
Erica Wright and Maggie Harrington <maggie@>
2017-03-13 15:17:51 -0700

    create files and buildBoard to display images

---
+++ js/memory.js
@@ -0,0 +1,42 @@
+function Game() {
+}
+
+Game.prototype.buildBoard = function() {
+  var pictures = [1,2,3,4,5,6,7,8];
+  var playPictures = pictures.concat(pictures.slice(0));
+
+  //Fischer-Yates Shuffle
+  function shuffle(playPictures) {
+  var currentIndex = playPictures.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle…
+  while (currentIndex) {
+    // Pick a remaining element…
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+      temporaryValue = playPictures[currentIndex];
+      playPictures[currentIndex] = playPictures[randomIndex];
+      playPictures[randomIndex] = temporaryValue;
+    }
+    console.log(playPictures);
+    return playPictures;
+  }
+
+  shuffle(playPictures);
+
+  var div = 1;
+  for (var i = 0; i < playPictures.length; i++) {
+      // $("#" + div).append(playPictures[i]);
+      $("#" + div).append("<img src='img/" + playPictures[i] + ".jpg'>");
+      div++;
+  }
+
+
+
+}
+
+
+$(document).ready(function() {
+  var game = new Game();
+  game.buildBoard();
+});
